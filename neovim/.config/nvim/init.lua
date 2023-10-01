68d75254ea9e08cf08e5c0383e0d5f0a82f70985
Tran Quang Loc <quangloctran99@gmail.com>
2023-10-01 17:08:54 +0700

    Only format on write

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -56,7 +56,7 @@ require("lazy").setup({
             vim.keymap.set("n", "<leader><C-i>", ":Neoformat<cr>", { silent = true })
             vim.g.neoformat_try_node_exe = 1
             vim.g.neoformat_enabled_typescript = { "prettier" }
-            vim.api.nvim_create_autocmd({ "BufWritePre", "TextChanged" }, {
+            vim.api.nvim_create_autocmd({ "BufWritePre" }, {
                 pattern = { "*.js", "*.ts" },
                 callback = function()
                     vim.cmd("Neoformat")
