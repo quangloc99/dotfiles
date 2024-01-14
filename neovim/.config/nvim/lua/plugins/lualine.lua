f0bd30edb11f891d6fd7051518aef9b9ca71d9e3
Tran Quang Loc <quangloctran99@gmail.com>
2024-01-14 18:30:25 +0700

    Add codeium

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -1,24 +1,34 @@
-return {
+local M = {
     "nvim-lualine/lualine.nvim",
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
+
     opts = function()
-        local function neomake_status()
-            if vim.fn.exists(":Neomake") == 0 then
-                return ""
+        local lualine_c = {}
+
+        if vim.fn.exists(":Neomake") ~= 0 then
+            local function neomake_status()
+                local curbuff = vim.fn.bufnr("%")
+                local res = "Neomake ("
+                    .. curbuff
+                    .. "): "
+                    .. vim.fn["neomake#statusline#get"](curbuff, {
+                        format_running = "… ({{running_job_names}})",
+                        format_loclist_ok = "✓",
+                        format_quickfix_ok = "",
+                        format_quickfix_issues = "%s",
+                    })
+                return res
             end
-            local curbuff = vim.fn.bufnr("%")
-            local res = "Neomake ("
-                .. curbuff
-                .. "): "
-                .. vim.fn["neomake#statusline#get"](curbuff, {
-                    format_running = "… ({{running_job_names}})",
-                    format_loclist_ok = "✓",
-                    format_quickfix_ok = "",
-                    format_quickfix_issues = "%s",
-                })
-            return res
+            table.insert(lualine_c, neomake_status)
+        end
+
+        if vim.fn.exists(":codeium#Complete()") then
+            local function codeium_status()
+                return vim.api.nvim_call_function("codeium#GetStatusString", {})
+            end
+            table.insert(lualine_c, codeium_status)
         end
 
         return {
@@ -31,7 +41,7 @@ return {
             sections = {
                 lualine_a = { "mode" },
                 lualine_b = { "branch", "diff", "diagnostics" },
-                lualine_c = { neomake_status },
+                lualine_c = lualine_c,
                 lualine_x = { "encoding", "fileformat", "filetype" },
                 lualine_y = { "progress" },
                 lualine_z = { "location" },
@@ -63,3 +73,5 @@ return {
         }
     end,
 }
+
+return M
