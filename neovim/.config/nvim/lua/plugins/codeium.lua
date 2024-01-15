2949a9e9fd7bcd16a5e29cf9e0aa44824d6b844b
Tran Quang Loc <quangloctran99@gmail.com>
2024-01-15 21:26:41 +0700

    Update codeium shortcuts

--- neovim/.config/nvim/lua/plugins/codeium.lua
+++ neovim/.config/nvim/lua/plugins/codeium.lua
@@ -8,15 +8,15 @@ return {
             return vim.fn["codeium#Accept"]()
         end, { expr = true, silent = true })
 
-        vim.keymap.set("i", "<c-;>", function()
+        vim.keymap.set("i", "<C-j>", function()
             return vim.fn["codeium#CycleCompletions"](1)
         end, { expr = true, silent = true })
 
-        vim.keymap.set("i", "<c-,>", function()
+        vim.keymap.set("i", "<C-k>", function()
             return vim.fn["codeium#CycleCompletions"](-1)
         end, { expr = true, silent = true })
 
-        vim.keymap.set("i", "<c-x>", function()
+        vim.keymap.set("i", "<C-x>", function()
             return vim.fn["codeium#Clear"]()
         end, { expr = true, silent = true })
     end,
