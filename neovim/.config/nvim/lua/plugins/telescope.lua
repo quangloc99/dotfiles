7d3015baa2595e84bc313e432af99bbcaa9bb0fd
Tran Quang Loc <quangloctran99@gmail.com>
2024-07-07 10:33:51 +0700

    Format telescope.lua

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,18 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    version = "^0.1.2",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
+    {
+        "nvim-telescope/telescope.nvim",
+        version = "^0.1.2",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        init = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
+            vim.keymap.set("n", "<leader>faf", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+        end,
     },
-    init = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
-        vim.keymap.set("n", "<leader>faf", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-        vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-    end,
+    { 'nvim-telescope/telescope-symbols.nvim' }
 }
