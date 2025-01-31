a31868e9d0861b0c65b217a4be75dbbc88bc54bd
Tran Quang Loc <quangloctran99@gmail.com>
2025-01-31 17:44:05 +0700

    [nvim] Add option to choose cargo run/build for rust

--- neovim/.config/nvim/lua/build_and_run_config/rust_build_and_run.lua
+++ neovim/.config/nvim/lua/build_and_run_config/rust_build_and_run.lua
@@ -11,17 +11,41 @@ function M.setup()
     utils.setup()
 end
 
+local mode_prompt = '&Cargo\n&Single file'
+
 function M.gen_makeprg()
-    local filename = vim.fn.expand("%:p")
-    local file_info = utils.get_file_info(filename)
-    return string.format(
-        'mkdir -p "%s" && rustc -O -D warnings -W dead-code -o "%s" "%s"',
-        file_info.build_dirname, file_info.exe_file, filename
-    )
+    local choice = vim.fn.confirm('Choose build mode:', mode_prompt)
+    if choice == 0 then
+        print('No preset is chosen')
+        return
+    end
+
+    if choice == 1 then
+        return 'RUSTFLAGS="--cfg LOCAL" cargo build'
+    else
+        local filename = vim.fn.expand("%:p")
+        local file_info = utils.get_file_info(filename)
+        return string.format(
+            'mkdir -p "%s" && rustc -O -D warnings -W dead-code -o "%s" "%s"',
+            file_info.build_dirname, file_info.exe_file, filename
+        )
+    end
 end
 
 function M.run_single_file_command()
-    return './%<'
+    local choice = vim.fn.confirm('Choose run mode:', mode_prompt)
+    if choice == 0 then
+        print('No preset is chosen')
+        return
+    end
+
+    if choice == 1 then
+        return 'RUSTFLAGS="--cfg LOCAL" cargo run'
+    else
+        local filename = vim.fn.expand("%:p")
+        local file_info = utils.get_file_info(filename)
+        return file_info.exe_file
+    end
 end
 
 require('single_file_runner').register_single_file_command(M)
